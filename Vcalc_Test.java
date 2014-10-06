1822d1403b36e77b6fdc1239c6a1f9abd7bea3fe
Artem Herasymchuk <artemh@gmail.com>
2014-10-05 21:06:08 -0600

    Cleanup of Vcalc_Test

--- Vcalc_Test.java
+++ Vcalc_Test.java
@@ -1,11 +1,15 @@
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Arrays;
 
-import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.ANTLRFileStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.TokenStream;
 import org.antlr.runtime.tree.BufferedTreeNodeStream;
+import org.antlr.stringtemplate.StringTemplate;
 import org.antlr.stringtemplate.StringTemplateGroup;
 /*
 import org.antlr.runtime.tree.DOTTreeGenerator;
@@ -15,47 +19,15 @@ import org.antlr.stringtemplate.StringTemplate;
 
 public class Vcalc_Test {
     public static void main(String[] args) throws RecognitionException {
-    /**
+    
 	if (args.length != 2) {
 	    System.err.print("Insufficient arguments: ");
 	    System.err.println(Arrays.toString(args));
 	    System.exit(1);
 	}
-	**/
-	//ANTLRFileStream input = null;
-    	/**
-	ANTLRStringStream input = new ANTLRStringStream("vector v1 = 1..10;\n" + 
-			"vector v2 = 2..8;\n" + 
-			"vector v3 = v1 * 2;\n" + 
-			"vector v4 = [i in (v1 * v2) + (v3 / 2) | i * 3 ];\n" + 
-			"vector v5 = filter(i in v4 | (i > 50) * (i < 100) );\n" + 
-			"print(v5);");
-			**/
 	
-    ANTLRStringStream input = new ANTLRStringStream("int i = 1;\n" + 
-    		"int p = 1;\n" + 
-    		"int isPrime = 1;\n" + 
-    		"\n" + 
-    		"loop (p < 1000)\n" + 
-    		"    i=1;\n" + 
-    		"    isPrime=1;\n" + 
-    		"    p=p+1;\n" + 
-    		"\n" + 
-    		"    loop (i < p/2)\n" + 
-    		"        i=i+1;\n" + 
-    		"\n" + 
-    		"        if ((p/i) * i == p)\n" + 
-    		"            isPrime = 0;\n" + 
-    		"            i = p;\n" + 
-    		"        fi;\n" + 
-    		"    pool;\n" + 
-    		"\n" + 
-    		"    if (isPrime)\n" + 
-    		"        print(p);\n" + 
-    		"    fi;\n" + 
-    		"pool;");
-    
-	/**
+	ANTLRFileStream input = null;
+	
 	try {
 	    input = new ANTLRFileStream(args[0]);
 	} catch (IOException e) {
@@ -63,54 +35,48 @@ public class Vcalc_Test {
 	    System.err.println(args[0]);
 	    System.exit(1);
 	}
-	**/
+	
 	vCalcLexer lexer = new vCalcLexer(input);
 	TokenStream tokenStream = new CommonTokenStream(lexer);
 	vCalcParser parser = new vCalcParser(tokenStream);
 	vCalcParser.program_return entry = parser.program();
 	Object ast = entry.getTree();
+	
 	//System.out.println(entry.tree.toStringTree());
+	
 	/*
 	DOTTreeGenerator gen = new DOTTreeGenerator();
     StringTemplate st = gen.toDOT((Tree) ast);
     System.out.println(st); 
     */
+	
 	BufferedTreeNodeStream nodes = new BufferedTreeNodeStream(ast);
 	nodes.setTokenStream(tokenStream);
 	
 	Validator validator = new Validator(nodes);
 	validator.program();
-	
-	nodes.reset();
-    //Interpreter interpreter = new Interpreter(nodes);
-    //interpreter.program();
-	Templater templater = new Templater(nodes);
-	String templateFile = "llvm.stg";
-
-    Reader template = new InputStreamReader(Vcalc_Test.class.getResourceAsStream(templateFile));
-    StringTemplateGroup stg = new StringTemplateGroup(template);
-    templater.setTemplateLib(stg);
-    System.out.println(templater.program().getTemplate().toString());
-	
-	/**
+		
 	if (args[1].equals("int")) {
 	    // Run it through the Interpreter
-	    // nodes.reset();
-	    //Interpreter interpreter = new Interpreter(nodes);
-	    //interpreter.program();
+	    nodes.reset();
+	    Interpreter interpreter = new Interpreter(nodes);
+	    interpreter.program();
 	}
 	else {
 	    // Pass it all to the String templater!
-	    //String templateFile = args[1] + ".stg";
+	    String templateFile = args[1] + ".stg";
 
-	    //Reader template = new InputStreamReader(Vcalc_Test.class.getResourceAsStream(templateFile));
-	    //StringTemplateGroup stg = new StringTemplateGroup(template);
+	    Reader template = new InputStreamReader(Vcalc_Test.class.getResourceAsStream(templateFile));
+	    StringTemplateGroup stg = new StringTemplateGroup(template);
+	    
+	    File f = new File(args[0]);
+	    
+	    stg.defineTemplate("filename", f.getName());
 
-	    // nodes.reset();
-	    //Templater templater = new Templater(nodes);
-	    //templater.setTemplateLib(stg);
-	    //System.out.println(templater.program().getTemplate().toString());
+	    nodes.reset();
+	    Templater templater = new Templater(nodes);
+	    templater.setTemplateLib(stg);
+	    System.out.println(templater.program().getTemplate().toString());
 	}
-	**/
     }
 }
